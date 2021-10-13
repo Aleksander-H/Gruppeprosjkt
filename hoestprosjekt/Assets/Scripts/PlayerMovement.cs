fb80acacc339e8c615be3badcff08d0f240f5850
UnluckyWeeb <batmanoscar01@gmail.com>
2021-10-13 12:51:43 +0200

    A test scene with a simple test map and updated player script  with an attack box prefab

--- hoestprosjekt/Assets/Scripts/PlayerMovement.cs
+++ hoestprosjekt/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer rend;
+
+    public float speed;
+    public float x;
+    public float y;
+
+    public bool isDead;
+    public bool isMoving;
+
+    public int playerHealth;
+
+    public GameObject attackHitbox;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if(isDead == false)
+        {
+            x = Input.GetAxis("Horizontal");
+            y = Input.GetAxis("Vertical");
+
+            rb.velocity = new Vector2(x, y) * speed;
+        }
+        
+    }
+
+    private void DeadPlayer()
+    {
+        isDead = true;
+    }
+
+    private void OnDestroy()
     {
         
     }
